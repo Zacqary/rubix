f01561445884aede61956d4df7869b1a11ddff0f
Daniel Cortes <d5t5c5@yahoo.com>
2019-01-08 17:54:23 -0600

    fix: fixed warning thrown from mapping
    line 74 would map through an array, but did not transform any
     values. Changed "map" to "forEach" to clean warning.

--- src/util/spinSlice.js
+++ src/util/spinSlice.js
@@ -71,7 +71,7 @@ export default (sideColors, sliceNumber, forward = true) => {
   const newSideColors = JSON.parse(JSON.stringify(sideColors));
   const sliceDef = sliceMap[sliceNumber];
   // Look up initial positions of the rotating rows/cols
-  const transforms = sliceDef.map(operation => {
+  const transforms = sliceDef.forEach(operation => {
     const [sideName, row, column] = operation;
     if (row !== null) {
         return sideColors[sideName][row];
